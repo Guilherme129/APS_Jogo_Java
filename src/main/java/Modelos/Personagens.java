9494798010e608b672fa9e6d57a3c7d58af2fbb8
Guilherme129 <guilhermecamargocorrea1@gmail.com>
2025-04-17 23:51:06 -0300

    modelo

--- src/main/java/Modelos/Personagens.java
+++ src/main/java/Modelos/Personagens.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+//Rodolfo
+
 package Modelos;
 
-/**
- *
- * @author guilherme
- */
+
 public class Personagens
 {
     
